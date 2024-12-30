007496e33b235be24a58fc248995371b8d9f07ed
Joshua <horheyinc@gmail.com>
2024-12-30 12:54:26 +0100

    changed all js to ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
+import { Application } from "express";
+
 // We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
 
@@ -16,7 +18,7 @@ const cors = require("cors");
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 
 // Middleware configuration
-module.exports = (app: any) => {
+module.exports = (app: Application) => {
   // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like Fly use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
